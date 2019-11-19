ba3077e857944fdb8f13d2654dc955e2382062a8
lukas leufen <l.leufen@fz-juelich.de>
2019-11-19 15:14:48 +0100

    corrected year in docstrings

--- printNumbers/parameters.py
+++ printNumbers/parameters.py
@@ -24,7 +24,7 @@
 #
 
 CONST_VERSION = 'V1.0'
-CONST_VERSION_STRING = '+ + PrintNumbers ' + CONST_VERSION + ' (Software Development in Science 2017) + +'
+CONST_VERSION_STRING = '+ + PrintNumbers ' + CONST_VERSION + ' (Software Development in Science 2019) + +'
 CONST_DEF_OPERAND_VAL = 10
 CONST_MAX_OPERAND_VAL = 20
 CONST_FUNC_CODE_FIBONACCI = 0
